e7ee4626c64273e9ed069d9212534535397eee3c
ALEJOYT <sochisdavid@gmail.com>
2025-08-06 20:37:56 +0000

    todavia no abre la camara que debo hacer

--- src/components/PigDoctorInputCard.tsx
+++ src/components/PigDoctorInputCard.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
