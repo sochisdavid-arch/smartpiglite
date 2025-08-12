8876dfea971a3c9f617da5702cbd1cd508f2dc7d
ALEJOYT <sochisdavid@gmail.com>
2025-08-12 21:48:33 +0000

    ya no lo publicare desde vercel sino aca en fire base ,cuando le doy a p

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database";
 import 'dotenv/config'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyD3TdAFwymZzevFIIKopTd4FMQ4ZXcjDiU",
+    authDomain: "smartpig-lite.firebaseapp.com",
+    projectId: "smartpig-lite",
+    storageBucket: "smartpig-lite.firebasestorage.app",
+    messagingSenderId: "487539621018",
+    appId: "1:487539621018:web:e483d82354ac0d9eea3323",
+    measurementId: ""
 };
 
 
