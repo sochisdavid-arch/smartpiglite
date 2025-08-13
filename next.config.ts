33b967d00b2dc902521303045facec482cf62a14
ALEJOYT <sochisdavid@gmail.com>
2025-08-13 03:50:57 +0000

    este es lo que aparece

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,9 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  }
 };
 
 export default nextConfig;
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI({
-    apiKey: "AIzaSyD3TdAFwymZzevFIIKopTd4FMQ4ZXcjDiU"
+    apiKey: process.env.GEMINI_API_KEY
   })],
   model: 'googleai/gemini-2.0-flash',
 });
