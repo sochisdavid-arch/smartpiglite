ff413f93a8c3623857cdafe104abb47957cb3e4f
ALEJOYT <sochisdavid@gmail.com>
2026-03-16 18:12:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "smartpig-lite",
+  "appId": "1:487539621018:web:e483d82354ac0d9eea3323",
+  "apiKey": "AIzaSyD3TdAFwymZzevFIIKopTd4FMQ4ZXcjDiU",
+  "authDomain": "smartpig-lite.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "487539621018"
+};
